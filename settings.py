33a75dfedfbf558184450e437294ffd8cbe371f1
j2xy <j2xy@yandex.com>
2021-02-19 15:03:33 +0800

    vs code 第一次

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """存储外星人入侵所有设置的类"""
+
+    def __init__(self):
+        """初始化游戏的设置"""
+        #屏幕设置
+        self.screen_width = 100
+        self.screen_height = 50
+        self.bg_color = (230, 0, 230)
